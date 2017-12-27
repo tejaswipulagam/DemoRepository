4aef8591173a43d820e1e0d1b6d854869857fbfd
Learning <Learning@DIN86000557.corp.capgemini.com>
2017-12-27 15:53:27 +0530

    Add project files.

---
+++ Demo/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+    }
+}
